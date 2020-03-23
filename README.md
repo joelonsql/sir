5782a3424fe6cddb66c824ca9b26408cdca23c9b
Joel Jakobsson <joel@compiler.org>
2020-03-23 01:08:48 +0100

    Add 2020-03-23 data

--- README.md
+++ README.md
@@ -13,18 +13,18 @@ Call:
 lm(formula = log2(IVAFall) ~ Dag, data = data)
 
 Residuals:
-     Min       1Q   Median       3Q      Max 
--0.34424 -0.14229  0.02434  0.11918  0.39733 
+    Min      1Q  Median      3Q     Max 
+-0.3367 -0.1820  0.0273  0.1642  0.3746 
 
 Coefficients:
             Estimate Std. Error t value Pr(>|t|)    
-(Intercept)  0.85986    0.10200    8.43 7.41e-07 ***
-Dag          0.35491    0.01055   33.65 8.55e-15 ***
+(Intercept)  0.90522    0.10530   8.597 3.51e-07 ***
+Dag          0.34735    0.01028  33.802 1.42e-15 ***
 ---
 Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
 
-Residual standard error: 0.1945 on 14 degrees of freedom
-Multiple R-squared:  0.9878,	Adjusted R-squared:  0.9869 
-F-statistic:  1132 on 1 and 14 DF,  p-value: 8.549e-15
+Residual standard error: 0.2076 on 15 degrees of freedom
+Multiple R-squared:  0.987,	Adjusted R-squared:  0.9862 
+F-statistic:  1143 on 1 and 15 DF,  p-value: 1.423e-15
 ```
 
--- sir.R
+++ sir.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(lubridate)
+library(plotly)
 
 # Helper function to show p-values
 signif.num <- function(x) {
@@ -9,7 +10,7 @@ signif.num <- function(x) {
 }
 
 # Daily data from: https://portal.icuregswe.org/siri/report/vtfstart-corona
-data <- data.frame(Dag = 1:16, IVAFall = cumsum(c(2,1,2,0,2,0,1,3,6,7,6,4,14,9,18,10)))
+data <- data.frame(Dag = 1:17, IVAFall = cumsum(c(2,1,2,0,2,0,1,3,6,7,6,4,14,9,18,10,6)))
 
 # Assume exponential curve
 model <- lm(log2(IVAFall) ~ Dag, data);
@@ -21,7 +22,7 @@ data$Model <- as.integer(2 ^ (data$Dag * model$coefficients[2] + model$coefficie
 data$Prediction <- NA
 
 # Predict next 7 days
-for (d in 17:23) {
+for (d in 18:24) {
   data <- add_row(
     data,
     Dag=d,
@@ -32,18 +33,26 @@ for (d in 17:23) {
 
 p_values <- sapply(model_summary$coefficients[,4],signif.num)
 
+data$Dag <- as.Date("2020-03-05") + data$Dag
+
 ggplot(data, aes(x=Dag)) +
   geom_point(aes(y=IVAFall, color="Historik")) +
   geom_line(aes(y=Model, color="Modell")) +
   geom_point(aes(y=Prediction, color="Prognos")) +
-  geom_ribbon(
-    aes(
-      ymin = Model - 2 * 2^model_summary$sigma,
-      ymax = Model + 2 * 2^model_summary$sigma,
-      fill="Modell"
-    ), alpha = 0.3, show.legend = FALSE) +
+#  geom_ribbon(
+#    aes(
+#      ymin = Model - 2 * 2^model_summary$sigma,
+#      ymax = Model + 2 * 2^model_summary$sigma,
+#      fill="Modell"
+#    ), alpha = 0.3, show.legend = FALSE) +
   scale_color_discrete(aesthetics = c("color", "fill")) +
-  geom_vline(xintercept = 16) +
+  geom_vline(xintercept = Sys.Date()) +
+  geom_text(aes(y = IVAFall, label = IVAFall),
+            vjust = "inward", hjust = "inward",
+            show.legend = FALSE, check_overlap = TRUE) +
+  geom_text(aes(y = Prediction, label = Prediction),
+            vjust = "inward", hjust = "inward",
+            show.legend = FALSE, check_overlap = TRUE) +
   theme_minimal() +
   ggtitle("Antal nyinskrivna intensivvårdtillfällen med Coronavirus i Sverige (kumulativt)",
     subtitle=paste(
@@ -51,3 +60,4 @@ ggplot(data, aes(x=Dag)) +
     )
   )
 
+model_summary
